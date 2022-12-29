bda5193e329a11bf21afd4466c9e3761dbfa30d3
tatsuya-yokoyama <ykym2011@gmail.com>
2022-12-29 10:19:57 +0900

    producer

---
+++ kafka-producer-sample/src/main/java/com/yokoyama/Producer.java
@@ -0,0 +1,31 @@
+package com.yokoyama;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+
+        String topic = "ads-log";
+        int campaignNum = 3;
+        for (int i = 0; i < campaignNum * 5; i++) {
+            int campaignId = i % campaignNum;
+            String ots = UUID.randomUUID().toString();
+            String event = String.format("ots=%s, campaign_id=%s", ots, campaignId);
+            producer.send(new ProducerRecord<>(topic, String.valueOf(campaignId), event));
+        }
+        producer.close();
+    }
+}
